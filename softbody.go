d124ded12d0c0a6d354cb4699d398f6bf9688370
Luis Gargitter <luis.gargitter@gmx.net>
2024-11-29 19:26:55 +0100

    dump.

--- softbody.go
+++ softbody.go
@@ -1,36 +1,3 @@
 package main
 
-import (
-	"github.com/go-gl/mathgl/mgl64"
-)
-
 type Softbody = SimpleUndirectedGraph[Particle, Link]
-
-const linkStride = linkOffsetDamper + 1
-
-const (
-	SoftbodyOffsetVerticesLength = 0
-	SoftbodyOffsetVertices       = 1
-)
-
-func (s *Softbody) toVecN() *mgl64.VecN {
-	size := 1 + len(s.vertices)*particleStride + len(s.edges)*linkStride
-	d := mgl64.NewVecN(size)
-
-	k := ParticleSystem(s.vertices)
-
-	d.Set(SoftbodyOffsetVerticesLength, float64(len(s.vertices)))
-	k.toVecN(d, SoftbodyOffsetVertices)
-
-	startEdges := SoftbodyOffsetVertices + len(s.vertices)*particleStride
-	for i, e := range s.edges {
-		VecNSetLink(d, startEdges+i*linkStride, &e.weight)
-	}
-
-	return d
-}
-
-func SoftbodyFromVecN(vn *mgl64.VecN) *Softbody {
-	p := ParticleSystemfromVecN(vn)
-
-}
